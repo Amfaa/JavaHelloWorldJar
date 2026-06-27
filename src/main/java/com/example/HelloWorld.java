f2118f070a5a5755223099be8d440fbd5e3c252d
Syed Mazhar Ali <87525103+Amfaa@users.noreply.github.com>
2026-06-28 01:20:39 +0530

    Update HelloWorld.java
    added in hello comment

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World from Java JAR!");
+        System.out.println("Hello World from Java JAR: Its a Jar Deployment!");
     }
 }
